2a7adc926dc24dca8fc522e59c41f247ab196575
Aleksandr.Poteryayko <Sanek191296@mail.ru>
2023-04-05 20:29:30 +0300

    develop - authCheck fix

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -13,20 +13,21 @@ httpApi.interceptors.request.use((config) => {
   return config;
 });
 
+let isRetryRefreshInterceptor: boolean = false;
 httpApi.interceptors.response.use(
   (config) => {
     return config;
   },
   async (error) => {
     const originalRequest = error.config;
-    if (error.response.status === 401 && error.config && !error.config.isRetry) {
-      originalRequest.isRetry = true;
+    if (error.response.status === 401 && error.config && !isRetryRefreshInterceptor) {
+      isRetryRefreshInterceptor = true;
       try {
         const response = await AuthService.refresh();
         localStorage.setItem('accessToken', response.data.accessToken);
         return httpApi.request(originalRequest);
       } catch (err) {
-        console.log('Сессия истекла');
+        console.log('Пользователь не авторизован');
       }
     }
     throw error;
